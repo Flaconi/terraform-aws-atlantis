35fed6a278bde50359f96acc59b7f155907eaf1f
runderwoodcr14 <ricardo.underwood@flaconi.de>
2020-11-16 09:31:24 +0100

    OPS-3968 Adding Overwrite option

--- main.tf
+++ main.tf
@@ -137,9 +137,10 @@ resource "aws_ssm_parameter" "webhook" {
 resource "aws_ssm_parameter" "atlantis_github_user_token" {
   count = var.atlantis_github_user_token != "" ? 1 : 0
 
-  name  = var.atlantis_github_user_token_ssm_parameter_name
-  type  = "SecureString"
-  value = var.atlantis_github_user_token
+  name      = var.atlantis_github_user_token_ssm_parameter_name
+  type      = "SecureString"
+  value     = var.atlantis_github_user_token
+  overwrite = true
 
   tags = local.tags
 }
